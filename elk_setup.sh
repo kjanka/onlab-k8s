dd6bb9b0b18df44ad2ce92ab29b5da4581af16ba
Janka Kovacs <42655516+kjanka@users.noreply.github.com>
2020-02-17 21:42:32 +0100

    Update elk_setup.sh

--- elk_setup.sh
+++ elk_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl create configmap logstash-config --from-file=logstash/pipeline/logstash.conf
+kubectl create configmap logstash-config --from-file=logstash.conf
 
 kubectl apply -f elasticsearch-deployment.yaml,elasticsearch-service.yaml,kibana-deployment.yaml,kibana-service.yaml,logstash-deployment.yaml,logstash-service.yaml
 
