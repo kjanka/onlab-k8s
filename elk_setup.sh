c07548954bd5596477f5c4b1a65970221105efbb
Janka Kovacs <42655516+kjanka@users.noreply.github.com>
2020-03-06 21:31:12 +0100

    Delete elk_setup.sh

--- elk_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl create configmap logstash-config --from-file=logstash.conf
-
-kubectl apply -f elasticsearch-deployment.yaml,elasticsearch-service.yaml,kibana-deployment.yaml,kibana-service.yaml,logstash-deployment.yaml,logstash-service.yaml
-
-echo "ELK stack setup complete"
